5613038e2a745135a58572f2f0ff76cbc7c6e696
Samir Tiwari <samirtiwari2061@gmail.com>
2024-06-15 00:35:03 +0545

    change favicon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,8 +10,8 @@ export default function Footer() {
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
+        React & Next.js, TypeScript, Tailwind CSS, Framer Motion, React Email &
+        Resend, Vercel hosting.
       </p>
     </footer>
   );

8a770dcf66ae7bcdf343e0a32fd36ab9a3609f31
Samir Tiwari <samirtiwari2061@gmail.com>
2025-04-20 16:48:29 +0545

    fix

--- components/Project.tsx
+++ components/Project.tsx
@@ -37,7 +37,7 @@ const Project = ({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 w-full  border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      <div className="bg-gray-100 w-full  border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col gap-5 h-full sm:group-even:ml-[18rem]">
           <div>
             <h3 className="text-2xl font-semibold">{title}</h3>
@@ -104,7 +104,7 @@ const Project = ({
 
         group-even:right-[initial] group-even:-left-40"
         />
-      </section>
+      </div>
     </motion.div>
   );
 };
